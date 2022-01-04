d5934b931df0d678f3be2f60e3d839cfb6353cba
Asep Bagja Priandana <bepitulaz@gmail.com>
2022-01-04 17:21:58 +0200

    Adding why relocate.

--- libs/utilities.ts
+++ libs/utilities.ts
@@ -1,15 +1,25 @@
-import remark from "remark"
-import html from "remark-html"
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeRaw from "rehype-raw";
+import rehypeStringify from "rehype-stringify";
 
-export function capitalize(string: string | string[] | undefined): string | string[] | undefined {
+export function capitalize(
+  string: string | string[] | undefined
+): string | string[] | undefined {
   if (typeof string === "string") {
-    return string.charAt(0).toUpperCase() + string.slice(1)
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  return string
+  return string;
 }
 
 export async function markdownToHtml(markdown: string): Promise<string> {
-  const result = await remark().use(html).process(markdown)
-  return result.toString()
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeStringify, { allowDangerousHtml: true })
+    .process(markdown);
+  return result.toString();
 }
