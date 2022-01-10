566cba369b5127973f1627c06611bc7f7329bb86
Asep Bagja Priandana <bepitulaz@gmail.com>
2022-01-10 23:32:05 +0200

    Preparation of music release.

--- components/BaseLayout.tsx
+++ components/BaseLayout.tsx
@@ -1,34 +1,38 @@
-import Col from "react-bootstrap/Col"
-import Container from "react-bootstrap/Container"
-import Navbar from "react-bootstrap/Navbar"
-import Nav from "react-bootstrap/Nav"
-import Row from "react-bootstrap/Row"
-import Link from "next/link"
-import {
-  Github,
-  Linkedin,
-  Twitter
-} from "react-bootstrap-icons"
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import Link from "next/link";
+import { Github, Linkedin, Twitter } from "react-bootstrap-icons";
+import { Category } from "@/libs/data-type";
+import { capitalize } from "@/libs/utilities";
 
-const BaseLayout = (props: any) : JSX.Element => {
-  const { children } = props
+const BaseLayout = (props: any): JSX.Element => {
+  const { children } = props;
 
   return (
-    <>
+    <div className="flex-wrapper">
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
           <Navbar.Brand href="/">Asep Bagja</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
             <Nav className="mr-auto">
-              <Link href="/business" passHref>
-                <Nav.Link>Business</Nav.Link>
+              <Link href={`/${Category.BUSINESS}`} passHref>
+                <Nav.Link>{capitalize(Category.BUSINESS)}</Nav.Link>
               </Link>
-              <Link href="/personal" passHref>
-                <Nav.Link>Personal</Nav.Link>
+              <Link href={`/${Category.MUSIC}`} passHref>
+                <Nav.Link>{capitalize(Category.MUSIC)}</Nav.Link>
               </Link>
-              <Link href="/programming" passHref>
-                <Nav.Link>Programming</Nav.Link>
+              <Link href={`/${Category.PERSONAL}`} passHref>
+                <Nav.Link>{capitalize(Category.PERSONAL)}</Nav.Link>
+              </Link>
+              <Link href={`/${Category.PROGRAMMING}`} passHref>
+                <Nav.Link>{capitalize(Category.PROGRAMMING)}</Nav.Link>
               </Link>
             </Nav>
           </Navbar.Collapse>
@@ -52,17 +56,29 @@ const BaseLayout = (props: any) : JSX.Element => {
             <Col xs={12} md={6}>
               <Nav className="justify-content-start justify-content-lg-end">
                 <Nav.Item>
-                  <Nav.Link className="text-white" href="https://github.com/bepitulaz" target="_blank">
+                  <Nav.Link
+                    className="text-white"
+                    href="https://github.com/bepitulaz"
+                    target="_blank"
+                  >
                     <Github />
                   </Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link className="text-white" href="https://twitter.com/bepitulaz" target="_blank">
+                  <Nav.Link
+                    className="text-white"
+                    href="https://twitter.com/bepitulaz"
+                    target="_blank"
+                  >
                     <Twitter />
                   </Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link className="text-white" href="https://linkedin.com/in/asepbagja" target="_blank">
+                  <Nav.Link
+                    className="text-white"
+                    href="https://linkedin.com/in/asepbagja"
+                    target="_blank"
+                  >
                     <Linkedin />
                   </Nav.Link>
                 </Nav.Item>
@@ -72,14 +88,16 @@ const BaseLayout = (props: any) : JSX.Element => {
           <Row>
             <Col>
               <div className="pt-3 mt-3 copyright-section">
-                <p className="text-center text-white fw-light">&copy; 2021 Asep Bagja Priandana</p>
+                <p className="text-center text-white fw-light">
+                  &copy; 2021 Asep Bagja Priandana
+                </p>
               </div>
             </Col>
           </Row>
         </Container>
       </footer>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default BaseLayout
+export default BaseLayout;
--- pages/[category]/index.tsx
+++ pages/[category]/index.tsx
@@ -1,85 +1,97 @@
-import { GetStaticProps, GetStaticPaths } from "next"
-import Head from "next/head"
-import { useRouter } from "next/router"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
-import Container from "react-bootstrap/Container"
-import SectionTitle from "../../components/SectionTitle"
-import { OneGrid, TwoGrids } from "../../components/PostCard"
-import BaseLayout from "../../components/BaseLayout"
-import { capitalize } from "../../libs/utilities"
-import { Article, Content, Category } from "../../libs/data-type"
-import { readFromFileSystem } from "../../libs/file-fetch"
+import { GetStaticProps, GetStaticPaths } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import SectionTitle from "../../components/SectionTitle";
+import { OneGrid, TwoGrids } from "../../components/PostCard";
+import BaseLayout from "../../components/BaseLayout";
+import { capitalize } from "../../libs/utilities";
+import { Article, Content, Category } from "../../libs/data-type";
+import { readFromFileSystem } from "../../libs/file-fetch";
 
 export const getStaticProps: GetStaticProps = async () => {
-  const idPosts = await readFromFileSystem(Content.ID)
-  const enPosts = await readFromFileSystem(Content.EN)
+  const idPosts = await readFromFileSystem(Content.ID);
+  const enPosts = await readFromFileSystem(Content.EN);
+  const featuredPosts = await readFromFileSystem(Content.FEATURED);
 
   return {
     props: {
-      data: idPosts.concat(enPosts),
+      data: idPosts.concat(enPosts).concat(featuredPosts),
     },
-  }
-}
+  };
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [
-      { params: { category: Category.BUSINESS }},
-      { params: { category: Category.PERSONAL }},
-      { params: { category: Category.PROGRAMMING }},
+      { params: { category: Category.BUSINESS } },
+      { params: { category: Category.PERSONAL } },
+      { params: { category: Category.PROGRAMMING } },
+      { params: { category: Category.MUSIC } },
     ],
     fallback: true,
-  }
-}
+  };
+};
 
 export default function CategoryPage(props: any): JSX.Element {
-  const { data } = props
-  const router = useRouter()
-  const { category } = router.query
+  const { data } = props;
+  const router = useRouter();
+  const { category } = router.query;
 
   const postsByCategory = data?.filter((post: Article) => {
-    const capitalCategory = capitalize(category)
+    const capitalCategory = capitalize(category);
 
     if (typeof capitalCategory === "string") {
-      return post.metadata?.categories?.includes(capitalCategory)
+      return post.metadata?.categories?.includes(capitalCategory);
     }
 
-    return post
-  })
+    return post;
+  });
 
   const ArticleLoop = (posts: any): JSX.Element => {
     const loopComponent = posts.data?.map((post: any, index: number) => {
-      let component = []
+      let component = [];
       if (index < 2) {
-        component.push(<TwoGrids
-          key={post.slug}
-          title={post.metadata.title}
-          excerpt={post.metadata.summary}
-          imageSrc={post.metadata.images?.[0]}
-          imageAlt={`the thumbnail of ${post.metadata.title}`}
-          href={`/${post.metadata.categories?.[0].toLowerCase()}/${post.slug}`}
-        />)
+        component.push(
+          <TwoGrids
+            key={post.slug}
+            title={post.metadata.title}
+            excerpt={post.metadata.summary}
+            imageSrc={post.metadata.images?.[0]}
+            imageAlt={`the thumbnail of ${post.metadata.title}`}
+            href={`/${post.metadata.categories?.[0].toLowerCase()}/${
+              post.slug
+            }`}
+          />
+        );
       } else {
-        component.push(<OneGrid
-          key={post.slug}
-          title={post.metadata.title}
-          excerpt={post.metadata.summary}
-          imageSrc={post.metadata.images?.[0]}
-          imageAlt={`the thumbnail of ${post.metadata.title}`}
-          href={`/${post.metadata.categories?.[0].toLowerCase()}/${post.slug}`}
-        />)
+        component.push(
+          <OneGrid
+            key={post.slug}
+            title={post.metadata.title}
+            excerpt={post.metadata.summary}
+            imageSrc={post.metadata.images?.[0]}
+            imageAlt={`the thumbnail of ${post.metadata.title}`}
+            href={`/${post.metadata.categories?.[0].toLowerCase()}/${
+              post.slug
+            }`}
+          />
+        );
       }
 
-      return component
-    })
+      return component;
+    });
 
-    return loopComponent ?? (
-      <Col>
-        <p className="text-center">No articles are found.</p>
-      </Col>
-    )
-  }
+    return (
+      loopComponent ?? (
+        <Col>
+          <p className="text-center">No articles are found.</p>
+        </Col>
+      )
+    );
+  };
 
   return (
     <BaseLayout>
@@ -87,26 +99,66 @@ export default function CategoryPage(props: any): JSX.Element {
         <title>{capitalize(category)} | Asep Bagja</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="My personal blog where I share my opinion and topic that I'm interested." />
+        <meta
+          name="description"
+          content="My personal blog where I share my opinion and topic that I'm interested."
+        />
         <meta property="og:type" content="website" />
-        <meta name="og:title" property="og:title" content={`${capitalize(category)} | Asep Bagja`} />
-        <meta name="og:description" property="og:description" content="My personal blog where I share my opinion and topic that I'm interested." />
+        <meta
+          name="og:title"
+          property="og:title"
+          content={`${capitalize(category)} | Asep Bagja`}
+        />
+        <meta
+          name="og:description"
+          property="og:description"
+          content="My personal blog where I share my opinion and topic that I'm interested."
+        />
         <meta property="og:site_name" content="The Blog of Asep Bagja" />
-        <meta property="og:url" content={`https://www.asepbagja.com/${category}`} />  
-        <meta name="twitter:card" content="summary" /> 
-        <meta name="twitter:title" content={`${capitalize(category)} | Asep Bagja`} />
-        <meta name="twitter:description" content="My personal blog where I share my opinion and topic that I'm interested." />
+        <meta
+          property="og:url"
+          content={`https://www.asepbagja.com/${category}`}
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta
+          name="twitter:title"
+          content={`${capitalize(category)} | Asep Bagja`}
+        />
+        <meta
+          name="twitter:description"
+          content="My personal blog where I share my opinion and topic that I'm interested."
+        />
         <meta name="twitter:site" content="@bepitulaz" />
         <meta name="twitter:creator" content="@bepitulaz" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
-        <meta property="og:image" content={`https://www.asepbagja.com${data?.[0].metadata.images?.[0]}`} />  
-        <meta name="twitter:image" content={`https://www.asepbagja.com${data?.[0].metadata.images?.[0]}`} />   
+        <meta
+          property="og:image"
+          content={`https://www.asepbagja.com${data?.[0].metadata.images?.[0]}`}
+        />
+        <meta
+          name="twitter:image"
+          content={`https://www.asepbagja.com${data?.[0].metadata.images?.[0]}`}
+        />
         <link rel="canonical" href={`https://www.asepbagja.com/${category}`} />
       </Head>
 
@@ -126,5 +178,5 @@ export default function CategoryPage(props: any): JSX.Element {
         </Container>
       </section>
     </BaseLayout>
-  )
+  );
 }
