3b6b0199cdec0fe2353cf21860c51a5f611bd190
Asep Bagja Priandana <bepitulaz@gmail.com>
2022-01-13 19:19:04 +0200

    Remove Google Analytics.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,9 @@
-import React, { useEffect } from "react";
-import { useRouter } from "next/router";
+import React from "react";
 import PlausibleProvider from "next-plausible";
-import * as ga from "../libs/ga";
 import type { AppProps } from "next/app";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      ga.pageview(url);
-    };
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <PlausibleProvider domain="asepbagja.com" trackOutboundLinks={true}>
       <Component {...pageProps} />
