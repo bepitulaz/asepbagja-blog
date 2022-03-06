3ed065ddbe4716b3774e2471593869807f878e35
Asep Bagja Priandana <bepitulaz@gmail.com>
2022-03-06 17:05:39 +0200

    Adding loading spinner in single page.

--- pages/[category]/[slug].tsx
+++ pages/[category]/[slug].tsx
@@ -5,6 +5,7 @@ import useTranslation from "next-translate/useTranslation";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Spinner from "react-bootstrap/Spinner";
 import { Article, CategoryID } from "@/libs/data-type";
 import { readFromFileSystem } from "@/libs/file-fetch";
 import BaseLayout from "@/components/BaseLayout";
@@ -89,7 +90,7 @@ const ReadingPage: NextPage<PageProps> = ({ data, locale }) => {
       ];
 
       const isCategoryID = listOfCategories.includes(category as CategoryID);
-      if(isCategoryID) {
+      if (isCategoryID) {
         Router.push(`/id/${category}/${slug}`);
       }
     }
@@ -97,130 +98,154 @@ const ReadingPage: NextPage<PageProps> = ({ data, locale }) => {
 
   return (
     <BaseLayout>
-      <Head>
-        <title>{article?.metadata.title} | Asep Bagja</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="description"
-          content={article?.metadata.summary || t("meta:no-description")}
-        />
-        <meta property="og:type" content="website" />
-        <meta
-          name="og:title"
-          property="og:title"
-          content={`${article?.metadata.title} | Asep Bagja`}
-        />
-        <meta
-          name="og:description"
-          property="og:description"
-          content={article?.metadata.summary || t("meta:no-description")}
-        />
-        <meta property="og:site_name" content={t("meta:sitename")} />
-        <meta
-          property="og:url"
-          content={`https://www.asepbagja.com${
-            lang === "en" ? "/" : "/" + lang + "/"
-          }${article?.metadata.categories?.[0].toLowerCase()}/${slug}`}
-        />
-        <meta name="twitter:card" content="summary" />
-        <meta
-          name="twitter:title"
-          content={`${article?.metadata.title} | Asep Bagja`}
-        />
-        <meta
-          name="twitter:description"
-          content={article?.metadata.summary || t("meta:no-description")}
-        />
-        <meta name="twitter:site" content="@bepitulaz" />
-        <meta name="twitter:creator" content="@bepitulaz" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta
-          property="og:image"
-          content={`https://www.asepbagja.com${article?.metadata?.images?.[0]}`}
-        />
-        <meta
-          name="twitter:image"
-          content={`https://www.asepbagja.com${article?.metadata?.images?.[0]}`}
-        />
-        <link
-          rel="canonical"
-          href={`https://www.asepbagja.com${
-            lang === "en" ? "/" : "/" + lang + "/"
-          }${article?.metadata?.categories?.[0].toLowerCase()}/${slug}`}
-        />
-      </Head>
+      {data ? (
+        <>
+          <Head>
+            <title>{article?.metadata.title} | Asep Bagja</title>
+            <meta charSet="utf-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+            <meta
+              name="description"
+              content={article?.metadata.summary || t("meta:no-description")}
+            />
+            <meta property="og:type" content="website" />
+            <meta
+              name="og:title"
+              property="og:title"
+              content={`${article?.metadata.title} | Asep Bagja`}
+            />
+            <meta
+              name="og:description"
+              property="og:description"
+              content={article?.metadata.summary || t("meta:no-description")}
+            />
+            <meta property="og:site_name" content={t("meta:sitename")} />
+            <meta
+              property="og:url"
+              content={`https://www.asepbagja.com${
+                lang === "en" ? "/" : "/" + lang + "/"
+              }${article?.metadata.categories?.[0].toLowerCase()}/${slug}`}
+            />
+            <meta name="twitter:card" content="summary" />
+            <meta
+              name="twitter:title"
+              content={`${article?.metadata.title} | Asep Bagja`}
+            />
+            <meta
+              name="twitter:description"
+              content={article?.metadata.summary || t("meta:no-description")}
+            />
+            <meta name="twitter:site" content="@bepitulaz" />
+            <meta name="twitter:creator" content="@bepitulaz" />
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link
+              rel="mask-icon"
+              href="/safari-pinned-tab.svg"
+              color="#5bbad5"
+            />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="theme-color" content="#ffffff" />
+            <meta
+              property="og:image"
+              content={`https://www.asepbagja.com${article?.metadata?.images?.[0]}`}
+            />
+            <meta
+              name="twitter:image"
+              content={`https://www.asepbagja.com${article?.metadata?.images?.[0]}`}
+            />
+            <link
+              rel="canonical"
+              href={`https://www.asepbagja.com${
+                lang === "en" ? "/" : "/" + lang + "/"
+              }${article?.metadata?.categories?.[0].toLowerCase()}/${slug}`}
+            />
+          </Head>
 
-      <main className="mt-5">
-        <Container>
-          <article>
+          <main className="mt-5">
+            <Container>
+              <article>
+                <Row>
+                  <Col lg={{ span: 8, offset: 2 }}>
+                    <div
+                      style={{
+                        paddingBottom: 5,
+                        borderBottom: "3px solid #343a40",
+                      }}
+                    >
+                      <div className="mb-3">
+                        <span className="badge bg-dark">
+                          {capitalize(article?.metadata.categories?.[0])}
+                        </span>
+                      </div>
+                      <h2>{article?.metadata.title}</h2>
+                      <p className="mt-2 fw-lighter fst-italic">
+                        {article?.metadata.summary}
+                      </p>
+                      <p
+                        className="fw-lighter lh-1"
+                        style={{ fontSize: "0.8rem" }}
+                      >
+                        <time dateTime={article?.date as string}>
+                          {article?.date}
+                        </time>
+                      </p>
+                    </div>
+                  </Col>
+                </Row>
+                <Row className="mt-5">
+                  <Col lg={{ span: 8, offset: 2 }}>
+                    <HtmlContent content={article?.content as string} />
+                  </Col>
+                </Row>
+              </article>
+              <section className="mt-3">
+                <Row>
+                  <Col lg={{ span: 8, offset: 2 }}>
+                    <CtaBox article={article} />
+                  </Col>
+                </Row>
+              </section>
+              <section className="mt-3">
+                <Row>
+                  <Col lg={{ span: 8, offset: 2 }}>
+                    <Discussion article={article} locale={locale} />
+                  </Col>
+                </Row>
+              </section>
+            </Container>
+          </main>
+        </>
+      ) : (
+        <main className="mt-5">
+          <Container>
             <Row>
-              <Col lg={{ span: 8, offset: 2 }}>
-                <div
-                  style={{
-                    paddingBottom: 5,
-                    borderBottom: "3px solid #343a40",
-                  }}
-                >
-                  <div className="mb-3">
-                    <span className="badge bg-dark">
-                      {capitalize(article?.metadata.categories?.[0])}
-                    </span>
-                  </div>
-                  <h2>{article?.metadata.title}</h2>
-                  <p className="mt-2 fw-lighter fst-italic">
-                    {article?.metadata.summary}
-                  </p>
-                  <p className="fw-lighter lh-1" style={{ fontSize: "0.8rem" }}>
-                    <time dateTime={article?.date as string}>
-                      {article?.date}
-                    </time>
-                  </p>
-                </div>
+              <Col xs={2} className="mx-auto text-center">
+                <Spinner animation="grow" variant="danger" title="Loading..." />
               </Col>
             </Row>
-            <Row className="mt-5">
-              <Col lg={{ span: 8, offset: 2 }}>
-                <HtmlContent content={article?.content as string} />
-              </Col>
-            </Row>
-          </article>
-          <section className="mt-3">
-            <Row>
-              <Col lg={{ span: 8, offset: 2 }}>
-                <CtaBox article={article} />
-              </Col>
-            </Row>
-          </section>
-          <section className="mt-3">
-            <Row>
-              <Col lg={{ span: 8, offset: 2 }}>
-                <Discussion article={article} locale={locale} />
-              </Col>
-            </Row>
-          </section>
-        </Container>
-      </main>
+          </Container>
+        </main>
+      )}
     </BaseLayout>
   );
 };
